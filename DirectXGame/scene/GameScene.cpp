90f4a0e7aee144d066bb015a8e7ce650369e56b5
So-Cyoki <146906786+So-Cyoki@users.noreply.github.com>
2024-04-15 20:36:25 +0900

    01_03_確認課題

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -1,19 +1,84 @@
 #include "GameScene.h"
+#include "AxisIndicator.h"
+#include "ImGuiManager.h"
+#include "PrimitiveDrawer.h"
 #include "TextureManager.h"
 #include <cassert>
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete _sprite;
+	delete _model;
+	delete _debugCamera;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	// Sprite
+	_textureHandle = TextureManager::Load("tex1.png");
+	_sprite = Sprite::Create(_textureHandle, {0, 0});
+
+	// Model
+	_textureHandle_model = TextureManager::Load("uvChecker.png");
+	_model = Model::Create();
+	_worldTransform.Initialize();
+	_viewProjection.Initialize();
+
+	// Sound
+	_soundDataHandle = audio_->LoadWave("fanfare.wav");
+	audio_->PlayWave(_soundDataHandle);
+	//_voiceHandle = audio_->PlayWave(_soundDataHandle, true);
+
+	// DebugCamera
+	_debugCamera = new DebugCamera(1280, 720);
+
+	// 3DLineDraw
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&_debugCamera->GetViewProjection());
+
+	// Axis
+	AxisIndicator::GetInstance()->SetVisible(true);
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&_debugCamera->GetViewProjection());
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	// Sprite Move
+	Vector2 pos = _sprite->GetPosition();
+	if (pos.x > 1280 - 128 || pos.x < 0) {
+		_speedX *= -1;
+	}
+	if (pos.y > 720 - 128 || pos.y < 0) {
+		_speedY *= -1;
+	}
+	pos.x += _speedX;
+	pos.y += _speedY;
+	_sprite->SetPosition(pos);
+
+	// Sound
+	if (input_->TriggerKey(DIK_SPACE)) {
+		audio_->StopWave(_voiceHandle);
+	}
+
+	// DebugText
+	ImGui::Begin("Model Scale");
+	ImGui::Text("XYZ = (%d/%d/%d)", int(_scaleFloat[0]), int(_scaleFloat[1]), int(_scaleFloat[2]));
+	ImGui::InputFloat3("InputFloat", _scaleFloat);
+	ImGui::SliderFloat3("SliderFloat", _scaleFloat, 0, 20);
+	// ImGui::ShowDemoWindow();
+	ImGui::End();
+
+	// Model
+	Matrix4x4 scaleMatrix = {_scaleFloat[0], 0, 0, 0, 0, _scaleFloat[1], 0, 0, 0, 0, _scaleFloat[2], 0, 0, 0, 0, 1};
+	_worldTransform.matWorld_ = scaleMatrix;
+	_worldTransform.TransferMatrix();
+
+	// DebugCamera
+	_debugCamera->Update();
+}
 
 void GameScene::Draw() {
 
@@ -42,8 +107,17 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 
+	_model->Draw(_worldTransform, _debugCamera->GetViewProjection(), _textureHandle_model);
+
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
+	// 3DLineDraw
+	for (int i = 0; i < 31; i++) {
+		PrimitiveDrawer::GetInstance()->DrawLine3d({-30, 0, 1.f * i}, {30, 0, 1.f * i}, {1, 0, 0, 1});
+		PrimitiveDrawer::GetInstance()->DrawLine3d({-30, 0, -1.f * i}, {30, 0, -1.f * i}, {1, 0, 0, 1});
+		PrimitiveDrawer::GetInstance()->DrawLine3d({1.f * i, 0, -30}, {1.f * i, 0, 30}, {0, 0, 1, 1});
+		PrimitiveDrawer::GetInstance()->DrawLine3d({-1.f * i, 0, -30}, {-1.f * i, 0, 30}, {0, 0, 1, 1});
+	}
 #pragma endregion
 
 #pragma region 前景スプライト描画
@@ -53,6 +127,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+
+	_sprite->Draw();
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Audio.h"
+#include "DebugCamera.h"
 #include "DirectXCommon.h"
 #include "Input.h"
 #include "Model.h"
@@ -43,6 +44,24 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
+
+	// Sprite
+	uint32_t _textureHandle = 0;
+	Sprite* _sprite = nullptr;
+	float _speedX = 1;
+	float _speedY = 1;
+	// Model
+	uint32_t _textureHandle_model = 0;
+	Model* _model = nullptr;
+	WorldTransform _worldTransform;
+	ViewProjection _viewProjection;
+	// Sound
+	uint32_t _soundDataHandle = 0;
+	uint32_t _voiceHandle = 0;
+	// ImGui
+	float _scaleFloat[3] = {1, 1, 1};
+	// DebugCamera
+	DebugCamera* _debugCamera = nullptr;
 
 	/// <summary>
 	/// ゲームシーン用
